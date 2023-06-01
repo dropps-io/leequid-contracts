64239b12284369bd6975b4c27cdc4d9df0890b80
samuel-videau <samuel.videau@efrei.net>
2023-06-01 16:32:20 +0200

    script: implementations deployment

---
+++ scripts/deploy_implementations.js
@@ -0,0 +1,64 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+  const args = {
+    gasPrice: '0x1DCD65000', // 8 Gwei
+  };
+
+  console.log('deploying RewardLyxToken...');
+  const RewardLyxToken = await ethers.getContractFactory('RewardLyxToken');
+  const rewardLyxToken = await RewardLyxToken.deploy(args);
+  await rewardLyxToken.deployed();
+  console.log(
+    'RewardLyxToken implementation deployed to:',
+    rewardLyxToken.address
+  );
+
+  console.log('deploying StakedLyxToken...');
+  const StakedLyxToken = await ethers.getContractFactory('StakedLyxToken');
+  const stakedLyxToken = await StakedLyxToken.deploy(args);
+  await stakedLyxToken.deployed();
+  console.log(
+    'StakedLyxToken implementation deployed to:',
+    stakedLyxToken.address
+  );
+
+  console.log('deploying Pool...');
+  const Pool = await ethers.getContractFactory('Pool');
+  const pool = await Pool.deploy(args);
+  await pool.deployed();
+  console.log('Pool implementation deployed to:', pool.address);
+
+  console.log('deploying PoolValidators...');
+  const PoolValidators = await ethers.getContractFactory('PoolValidators');
+  const poolValidators = await PoolValidators.deploy(args);
+  await poolValidators.deployed();
+  console.log(
+    'PoolValidators implementation deployed to:',
+    poolValidators.address
+  );
+
+  console.log('deploying MerkleDistributor...');
+  const MerkleDistributor = await ethers.getContractFactory(
+    'MerkleDistributor'
+  );
+  const merkleDistributor = await MerkleDistributor.deploy(args);
+  await merkleDistributor.deployed();
+  console.log(
+    'MerkleDistributor implementation deployed to:',
+    merkleDistributor.address
+  );
+
+  console.log('deploying Oracles...');
+  const Oracles = await ethers.getContractFactory('Oracles');
+  const oracles = await Oracles.deploy(args);
+  await oracles.deployed();
+  console.log('Oracles implementation deployed to:', oracles.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
